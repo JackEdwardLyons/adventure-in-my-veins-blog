984aad91bd83ed93752ac3bb60f6f4dc4ff9c837
Jack Lyons <jack.edward.lyons@gmail.com>
2021-09-16 09:32:25 +1000

    Chore: Remove instagram plugin as it's not working anymore

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,69 +5,59 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Adventure In My Veins',
-  siteDescription: 'An adventure publication crafted for wanderlusting, mountain lovers who want to build a location independent lifestyle.',
+  siteName: "Adventure In My Veins",
+  siteDescription:
+    "An adventure publication crafted for wanderlusting, mountain lovers who want to build a location independent lifestyle.",
 
   templates: {
-    Post: '/:title',
-    Tag: '/tag/:id'
+    Post: "/:title",
+    Tag: "/tag/:id",
   },
 
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
   },
 
   plugins: [
     {
-      use: '@zefman/gridsome-source-instagram',
+      use: "@gridsome/plugin-google-analytics",
       options: {
-        username: 'adventureinmyveins', // Instagram username
-        typeName: 'InstagramPhoto' // The GraphQL type you want the photos to be added under. Defaults to InstagramPhoto
-      }
-    },
-    {
-      use: '@gridsome/plugin-google-analytics',
-      options: {
-        id: 'UA-155581906-1'
-      }
+        id: "UA-155581906-1",
+      },
     },
     {
       // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
+        typeName: "Post",
+        path: "content/posts/*.md",
         refs: {
           // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
           tags: {
-            typeName: 'Tag',
+            typeName: "Tag",
             route: "/tag/:id",
-            create: true
-          }
-        }
-      }
+            create: true,
+          },
+        },
+      },
     },
     {
       use: `gridsome-plugin-netlify-cms`,
       options: {
-        publicPath: `/admin`
-      }
-    }
+        publicPath: `/admin`,
+      },
+    },
   ],
 
   transformers: {
     //Add markdown support to all file-system sources
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
-    }
-  }
-}
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: ["@gridsome/remark-prismjs"],
+    },
+  },
+};
